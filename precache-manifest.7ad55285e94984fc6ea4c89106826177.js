6b4ca264649a36a1b6a1aeb3dce2b8a1a3f989bb
Chau Vu <vuhachau2412@gmail.com>
2023-12-25 15:32:13 +0700

    Updates

---
+++ precache-manifest.7ad55285e94984fc6ea4c89106826177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0086d196b2a6a430b8605e9c1c97e07",
+    "url": "/Viet-Sign-Language-Detection-Numbers/index.html"
+  },
+  {
+    "revision": "0655177b3ad0eb03908e",
+    "url": "/Viet-Sign-Language-Detection-Numbers/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1502c3abded48f62f05",
+    "url": "/Viet-Sign-Language-Detection-Numbers/static/js/2.d200e498.chunk.js"
+  },
+  {
+    "revision": "93ce851949c81c033c28f6758a3c33f3",
+    "url": "/Viet-Sign-Language-Detection-Numbers/static/js/2.d200e498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0655177b3ad0eb03908e",
+    "url": "/Viet-Sign-Language-Detection-Numbers/static/js/main.07e1f2f5.chunk.js"
+  },
+  {
+    "revision": "3b10a6c7dae7c881d880",
+    "url": "/Viet-Sign-Language-Detection-Numbers/static/js/runtime-main.97832c8e.js"
+  }
+]);
